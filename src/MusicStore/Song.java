10e21b3f365e78999f5201609a8aa1ce880e26bf
Lufferly <pug.pugface@gmail.com>
2025-02-12 18:05:28 -0700

    added name

--- src/MusicStore/Song.java
+++ src/MusicStore/Song.java
@@ -1,9 +1,19 @@
 package MusicStore;
 
 public class Song {
+	/// The name of the song
+	private String name;
+	
+	/// Whether the song has been favorited or not
 	private boolean favorite;
 	
-	public Song() {
+	public Song(String songName) {
+		name = songName;
 		favorite = false;
+	}
+	
+	// Returns the name of the song
+	public String getName() {
+		return name;
 	}
 }
