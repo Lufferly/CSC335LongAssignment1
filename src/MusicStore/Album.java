2047a0bd370b0a57ca04b1b53152c25ac0ca71f5
Matteo Galiano <mgaliano2811@dhcp-10-134-229-62.uawifi.arizona.edu>
2025-02-12 18:29:12 -0700

    Added getters

--- src/MusicStore/Album.java
+++ src/MusicStore/Album.java
@@ -1,5 +1,38 @@
 package MusicStore;
 
-public class Album {
+import java.util.ArrayList;
 
+public class Album {
+	private String name;
+	private String author;
+	private String genre;
+	private int year;
+	private ArrayList<Song> songs;
+	
+	public Album(String name, String author, String genre, int year,
+			ArrayList<Song> songs) {
+		this.name = name;
+		this.author = author;
+		this.genre = genre;
+		this.year = year;
+		this.songs = new ArrayList<Song> (songs);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	
 }
