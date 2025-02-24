5025423c5471c993f4e4c0c7cf9c656540ff4d7b
Lufferly <pug.pugface@gmail.com>
2025-02-23 22:25:42 -0700

    add junit

---
+++ src/test/AlbumTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals; 
+
+import org.junit.jupiter.api.Test;
+
+public class AlbumTest {
+	@Test
+	void testInt() {
+		assertEquals(3 + 4, 7);
+	}
+
+}
