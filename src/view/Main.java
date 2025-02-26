a249e161bf672c4259837923af3161beff2b0f22
Lufferly <pug.pugface@gmail.com>
2025-02-26 00:34:58 -0700

    Main searching functionality

--- src/view/Main.java
+++ src/view/Main.java
@@ -11,6 +11,27 @@ public class Main {
     public static void main(String args[]) {
         // Create all of the albums
         MusicStore musicStore = new MusicStore("data/albums.txt");
-        musicStore.printAlbums();
+        // Create the view
+        View view = new View();
+
+        // The user input broken into a list of words
+        ArrayList<String> userInput = new ArrayList<>();
+        // The main loop
+        while (true) {
+            // Get the command from the user
+            userInput.clear();
+            for (String word : view.getInput().split(" ")) {
+                userInput.add(word.toLowerCase()); // Make everything lowercase, for simplification
+            }
+
+            // Search functionality
+            if (userInput.get(0).equals("search")) {
+                System.out.println("searching...");
+                System.out.println(view.search(userInput, musicStore));
+            }
+            if (userInput.get(0).equals("buy")) {
+                System.out.println("buying...");
+            }
+        }
     }
 }
